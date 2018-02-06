5dc8263247c5ab07abf290f709ac858c385b1e1d
jepson <jepson@jepsondeMacBook-Pro.local>
2018-02-06 11:01:41 +0800

    remove vender

--- vendor/evaisse/php-json-schema-generator/tests/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-error_reporting(E_ALL | E_STRICT);
-
-$vendorDir = __DIR__ . '/../vendor';
-$loader = require $vendorDir . '/autoload.php';
--- vendor/league/json-reference/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Change Log
-
-## 1.0.0 - 2017-04-29
-
-Initial release of league/json-reference as a standalone package.
